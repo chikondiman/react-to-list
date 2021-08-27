12f8a06ae0548ded8ea2adc7fc79d3962ce7ad40
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-27 05:43:14 -0400

    Update TodoForm.js

---
+++ src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import { falseFunc } from "boolbase";
+import React from "react";
+
+function TodoForm() {
+    const [todo, setTodo] = useState ({
+        id:"",
+        task: "",
+        completed: false
+
+
+    });
+    return (
+        <form>
+           <input/>
+
+           <button/>
+        </form>
+    )
+}
