0730a419f5544076d5277226297fe99090ca1080
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-29 12:08:26 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import React, { useState } from "react;"
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState([]);
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,12 @@ import React from "react";
 
 function Todo({ todo }) {
     return (
+        <div>
         <input type="checkbox" />
         <li>{todo.task}</li>
         <button>X</button>
+
+        </div>
     );
 }
 
