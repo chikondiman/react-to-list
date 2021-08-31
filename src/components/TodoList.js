f6649cd0c22f400bbb2406dcc24329c7421a143b
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-31 09:49:19 -0400

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,13 +2,13 @@ import React from "react";
 import Todo from "./Todo"
 
 
-function TodoList({ todos })
+function TodoList({ todos, toggleComplete, removeTodo })
 {
 return (
 <ul>
 
 {todos.map(todo => (
-    <Todo key={todo.id} todo={todo} toggleComplete={toggleComplete} />
+    <Todo key={todo.id} todo={todo} toggleComplrete={toggleComplete} />
 ))}
 
 
