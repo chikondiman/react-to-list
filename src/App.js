5187575149e521f310d8b22d5c1a445cb59bee81
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-01 10:23:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import './App.css';
-
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   const [todos, setTodos] = useState([]);
