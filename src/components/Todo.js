b862d547c89d001079404d95f93e1d963f9bbf73
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-02 10:19:43 -0400

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Checkbox, IconButton, ListItem, Typography} from "@material-ui/core"
 import CloseIcon from "@material-ui/icons/Close";
+import createTypography from "@material-ui/core/styles/createTypography";
 function Todo({ todo, toggleComplete, removeTodo }) {
     function handleCheckboxClick(){
         toggleComplete(todo.id);
@@ -12,7 +13,7 @@ function Todo({ todo, toggleComplete, removeTodo }) {
     return (
         <div style={{ display: "flex"}}>
         <input type="checkbox" onClick={handleCheckboxClick}/>
-        <li
+        <Typography
         style={{
             color: "white",
             textDecoration: todo.completed ? "line-through" : null
